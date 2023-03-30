5da4747f572e75576b2cca9b5a5314128cefc4a1
mazhu <mazhu@zoneyet.com>
2023-03-30 15:39:01 +0800

    添加入口

--- AIArm/PersonViewController.swift
+++ AIArm/PersonViewController.swift
@@ -6,24 +6,68 @@
 //
 
 import UIKit
+import SnapKit
 
-class PersonViewController: BaseViewController {
+class PersonViewController: UIViewController {
+
+    // MARK: - Properties
+
+    private let addPersonButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("添加就诊人", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(addPerson), for: .touchUpInside)
+        return button
+    }()
+
+    private let personListButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("就诊人列表", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(showPersonList), for: .touchUpInside)
+        return button
+    }()
+
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    // MARK: - Selectors
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func addPerson() {
+        let addPersonVC = AddPersonViewController()
+        navigationController?.pushViewController(addPersonVC, animated: true)
     }
-    */
 
+    @objc func showPersonList() {
+        let personListVC = PersonListViewController()
+        navigationController?.pushViewController(personListVC, animated: true)
+    }
+
+    // MARK: - Helpers
+
+    func configureUI() {
+        view.backgroundColor = .white
+
+        view.addSubview(addPersonButton)
+        addPersonButton.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.centerY.equalTo(view).offset(-50)
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+        }
+
+        view.addSubview(personListButton)
+        personListButton.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.centerY.equalTo(view).offset(50)
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+        }
+    }
 }
